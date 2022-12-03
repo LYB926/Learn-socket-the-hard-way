d77abd7db0659b419637d980832b7bac2917a86a
Wisp <zk1019@outlook.com>
2022-12-03 22:49:44 +0800

    多线程实现

--- Client/client.py
+++ Client/client.py
@@ -1,5 +1,7 @@
 # === TCP Client ===
 from socket import *
+import time
+from random import randrange
 
 IP = '127.0.0.1'
 SERVER_PORT = 50000
@@ -12,14 +14,15 @@ dataSocket = socket(AF_INET, SOCK_STREAM)
 dataSocket.connect((IP, SERVER_PORT))
 
 while True:
-    toSendMsg = input('>>>' )            # 从终端读取用户输入
-    if toSendMsg == 'disconnect':
+    #toSendMsg = input('>>>' )            # 从终端读取用户输入
+    toSendMsg = str(randrange(0, 20))
+    if toSendMsg == '114514':
         break
     dataSocket.send(toSendMsg.encode())  # 编码发送消息
-
-    recvMsg = dataSocket.recv(BUFLEN)    # 接收Server返回的消息
-    if not recvMsg:
-        break
-    print(recvMsg.decode())              # 打印读取到的消息
+    time.sleep(1)
+    # recvMsg = dataSocket.recv(BUFLEN)    # 接收Server返回的消息
+    # if not recvMsg:
+    #    break
+    # print(recvMsg.decode())              # 打印读取到的消息
 
 dataSocket.close()
--- flask/web_server.py
+++ flask/web_server.py
@@ -1,12 +1,48 @@
+# 后端Flask使用库
 from random import randrange
-
 from flask import Flask, render_template
-
 from pyecharts import options as opts
 from pyecharts.charts import Line
-
 from time import strftime
 
+# Socket库和配置 
+import threading
+from socket import *
+msg_num = 0
+def socket_thr():
+    IP = ''         # 主机地址为空，表示绑定本机所有网络接口IP地址
+    PORT = 50000    # 服务端口号
+    BUFLEN = 512    # Socket缓冲区大小
+    # 实例化Socket对象
+    listenSocket = socket(AF_INET, SOCK_STREAM) # AF_INET表示网络层使用IP协议，SOCK_STREAM表示传输层使用TCP协议
+    listenSocket.bind((IP, PORT))               # 绑定IP和端口
+    # 使Socket进入监听状态，最多接受8个等待连接的Client
+    listenSocket.listen(8)
+    print(f'Server listening at port {PORT}')
+
+    dataSocket, addr = listenSocket.accept()
+    print('Client connected:', addr)
+
+    while True:
+        recv_msg = dataSocket.recv(BUFLEN)  # 读取Client发送的消息
+        if not recv_msg:                    # 如果消息为空表示连接已关闭，退出循环    
+            break
+        
+        global msg_num
+        msg = recv_msg.decode()             # 将收到的消息解码为字符串
+        msg_num = float(msg)
+        print(f'Message received: {msg}')
+        # 编码，向Client发送消息
+        # dataSocket.send(f'Server message received: {msg}'.encode())
+
+    # 调用close()关闭Socket
+    dataSocket.close()
+    listenSocket.close()
+
+socket_thread = threading.Thread(target=socket_thr)
+socket_thread.start()
+
+# 后端工作函数
 app = Flask(__name__, static_folder="templates")
 
 timeList = [strftime('%H:%M:%S') for __ in range(32)]
@@ -16,9 +52,11 @@ def Line_base() -> Line:
     timeIndex = strftime('%H:%M:%S')
     timeList.pop(0)
     timeList.append(timeIndex)
-    dataNext = randrange(0, 20)
+    # dataNext = randrange(0, 20)
+    dataNext = msg_num
     dataList.pop(0)
     dataList.append(dataNext)
+    # print (msg_num+1.5)
     c = (
         Line()
         .add_xaxis(timeList)
